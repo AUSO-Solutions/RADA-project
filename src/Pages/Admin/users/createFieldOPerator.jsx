c4f5df9a23bd64ea47c3520b30d01cc2c5a88366
geediegram <geediegramgu@gmail.com>
2024-05-04 23:17:40 +0100

    fixes to logic

--- src/Pages/Admin/users/createFieldOPerator.jsx
+++ src/Pages/Admin/users/createFieldOPerator.jsx
@@ -16,12 +16,12 @@ const CreateFieldOPerator = () => {
 
     return (
         <RadaForm btnClass={'w-[fit-content]'} className={'flex flex-col justify-center'} validationSchema={schema} btnText={'Create Field Operator'} url={'/admin/create-fieldOperator'} method={'post'} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '500px', gap: '20px' }} >
-            <Input name='superAdminEmail' hidden value={'eoludairo61@gmail.com'} />
+            <Input name='superAdminEmail' hidden value={'kehindesalaudeen222@gmail.com'} />
             <Input label={'First Name'} name='firstName' />
             <Input label={'Last Name'} name='lastName' />
             <Input label={'Email'} name='email' />
             <Input label={'Asset'} name='assetType' />
-           
+
 
 
         </RadaForm>
