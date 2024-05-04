2e597d89edd36066ddb874aa10cf7b4563846cc9
kenicodex <kehindesalaudeen222@gmail.com>
2024-05-05 00:10:09 +0100

    field operator

--- src/Pages/Admin/users/createFieldOPerator.jsx
+++ src/Pages/Admin/users/createFieldOPerator.jsx
@@ -2,23 +2,31 @@
 import { Input, RadaForm } from 'Components'
 import * as Yup from 'yup';
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 const CreateFieldOPerator = () => {
 
     // const navigate = useNavigate();
+    const state = useSelector(state => state.auth?.user)
+    // console.log(state)
 
-    const schema= Yup.object().shape({
+    const schema = Yup.object().shape({
         email: Yup.string().required(),
         // password: Yup.string().required().min(8),
     })
 
     return (
-        <RadaForm btnClass={'w-[fit-content]'} className={'flex flex-col justify-center'} validationSchema={schema} btnText={'Create Field Operator'} url={'/admin/create-fieldOperator'} method={'post'} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '500px', gap: '20px' }} >
-            <Input label={'First Name'} name='adminEmail' hidden value={'eoludairo61@gmail.com'} />  
-             <Input label={'First Name'} name='firstname' />
-            <Input label={'Last Name'} name='firstname' />
+        <RadaForm
+            btnClass={'w-[fit-content]'}
+            className={'flex flex-col justify-center'}
+            validationSchema={schema} btnText={'Create Field Operator'} url={'/api/admin/create-fieldOperator'}
+            method={'post'}
+            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '500px', gap: '20px' }} >
+            <Input label={''} name='superAdminEmail' hidden value={state?.data?.email} />
+            <Input label={'First Name'} name='firstName' />
+            <Input label={'Last Name'} name='lastName' />
             <Input label={'Username'} name='email' />
-            <Input label={'Asset'} name='asset' />      
+            <Input label={'Asset'} name='asset' />
             <div style={{ display: 'flex', justifyContent: 'space-between', gap: '300px' }} >
                 {/* <Button width={'100px'} shadow onClick={() => window.location.pathname.includes('152') ? navigate('/152/register') : window.location.pathname.includes('147') ? navigate('/147/register') : navigate('/24/register')} >
                     Register
@@ -28,7 +36,7 @@ const CreateFieldOPerator = () => {
                 </Button> */}
             </div>
 
-          
+
         </RadaForm>
     )
 }
--- src/Pages/Admin/users/createQAQC.jsx
+++ src/Pages/Admin/users/createQAQC.jsx
@@ -10,7 +10,7 @@ const CreateQAQC = () => {
 
     // const navigate = useNavigate();
     const state = useSelector(state => state.auth)
-    // console.log(state?.user)
+    console.log(state?.user)
 
     const schema = Yup.object().shape({
         email: Yup.string().required(),
@@ -20,7 +20,7 @@ const CreateQAQC = () => {
     return (
         <RadaForm validationSchema={schema} btnText={'Create QA/QC'} url={'/api/admin/create-qaQc'} method={'post'} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '500px', gap: '20px' }} >
             <Input label={''} name='superAdminEmail' hidden value={state?.user?.data?.email} />
-            <Input label={'First Name'} name='firstname' />
+            <Input label={'First Name'} name='firstName' />
             <Input label={'Last Name'} name='lastName' />
             <Input label={'Username'} name='email' />
             <Input label={'Asset'} name='asset' />
