3a0e45e49759bdb91e1992438ae99f1d1a650817
geediegram <geediegramgu@gmail.com>
2024-05-04 22:33:30 +0100

    fixes

--- src/Pages/Admin/users/createFieldOPerator.jsx
+++ src/Pages/Admin/users/createFieldOPerator.jsx
@@ -9,28 +9,21 @@ const CreateFieldOPerator = () => {
 
     // const navigate = useNavigate();
 
-    const schema= Yup.object().shape({
+    const schema = Yup.object().shape({
         email: Yup.string().required(),
         // password: Yup.string().required().min(8),
     })
 
     return (
         <RadaForm btnClass={'w-[fit-content]'} className={'flex flex-col justify-center'} validationSchema={schema} btnText={'Create Field Operator'} url={'/admin/create-fieldOperator'} method={'post'} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '500px', gap: '20px' }} >
-            <Input  name='superAdminEmail' hidden value={'eoludairo61@gmail.com'} />  
-             <Input label={'First Name'} name='firstname' />
-            <Input label={'Last Name'} name='lastname' />
-            <Input label={'Username'} name='email' />
-            <Input label={'Asset'} name='assetType' />      
-            <div style={{ display: 'flex', justifyContent: 'space-between', gap: '300px' }} >
-                {/* <Button width={'100px'} shadow onClick={() => window.location.pathname.includes('152') ? navigate('/152/register') : window.location.pathname.includes('147') ? navigate('/147/register') : navigate('/24/register')} >
-                    Register
-                </Button> */}
-                {/* <Button width={'100px'} shadow onClick={() => login({ email: '', "password": "" })} >
-                    Login
-                </Button> */}
-            </div>
+            <Input name='superAdminEmail' hidden value={'eoludairo61@gmail.com'} />
+            <Input label={'First Name'} name='firstName' />
+            <Input label={'Last Name'} name='lastName' />
+            <Input label={'Email'} name='email' />
+            <Input label={'Asset'} name='assetType' />
+           
 
-          
+
         </RadaForm>
     )
 }
--- src/Pages/Admin/users/createQA_QC.jsx
+++ src/Pages/Admin/users/createQA_QC.jsx
@@ -9,27 +9,20 @@ const CreateQA_QC = () => {
 
     // const navigate = useNavigate();
 
-    const schema= Yup.object().shape({
+    const schema = Yup.object().shape({
         email: Yup.string().required(),
         // password: Yup.string().required().min(8),
     })
 
     return (
-        <RadaForm validationSchema={schema} btnText={'Create QA/QC'} url={'/admin/create-qaQc'} method={'post'} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '500px', gap: '20px' }} >
-            <Input label={'First Name'} name='firstname' />
-            <Input label={'Last Name'} name='firstname' />
+        <RadaForm btnClass={'w-[fit-content]'} className={'flex flex-col justify-center'} validationSchema={schema} btnText={'Create QA/QC'} url={'/admin/create-qaQc'} method={'post'} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '500px', gap: '20px' }} >
+            <Input name='superAdminEmail' hidden value={'eoludairo61@gmail.com'} />
+            <Input label={'First Name'} name='firstName' />
+            <Input label={'Last Name'} name='lastName' />
             <Input label={'Username'} name='email' />
-            <Input label={'Asset'} name='asset' />
-            <div style={{ display: 'flex', justifyContent: 'space-between', gap: '300px' }} >
-                {/* <Button width={'100px'} shadow onClick={() => window.location.pathname.includes('152') ? navigate('/152/register') : window.location.pathname.includes('147') ? navigate('/147/register') : navigate('/24/register')} >
-                    Register
-                </Button> */}
-                {/* <Button width={'100px'} shadow onClick={() => login({ email: '', "password": "" })} >
-                    Login
-                </Button> */}
-            </div>
+            <Input label={'Asset'} name='assetType' />
 
-          
+
         </RadaForm>
     )
 }
