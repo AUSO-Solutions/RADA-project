7c05b6c4a159191c8b417f40f155eefbeaf0d36f
geediegram <geediegramgu@gmail.com>
2024-05-05 10:29:08 +0100

    g-dev update

--- src/Pages/Admin/users/createFieldOPerator.jsx
+++ src/Pages/Admin/users/createFieldOPerator.jsx
@@ -15,13 +15,17 @@ const CreateFieldOPerator = () => {
     })
 
     return (
-        <RadaForm btnClass={'w-[fit-content]'} className={'flex flex-col justify-center'} validationSchema={schema} btnText={'Create Field Operator'} url={'/admin/create-fieldOperator'} method={'post'} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '500px', gap: '20px' }} >
-            <Input name='superAdminEmail' hidden value={'kehindesalaudeen222@gmail.com'} />
+        <RadaForm
+            btnClass={'w-[fit-content]'}
+            className={'flex flex-col justify-center'}
+            validationSchema={schema} btnText={'Create Field Operator'} url={'/api/admin/create-fieldOperator'}
+            method={'post'}
+            style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '500px', gap: '20px' }} >
+            <Input label={''} name='superAdminEmail' hidden value={state?.data?.email} />
             <Input label={'First Name'} name='firstName' />
             <Input label={'Last Name'} name='lastName' />
-            <Input label={'Email'} name='email' />
-            <Input label={'Asset'} name='assetType' />
-
+            <Input label={'Username'} name='email' />
+            <Input label={'Asset'} name='asset' />
 
 
         </RadaForm>
