8477737572f0e105cd5f370c5ebfe5b4738df8e4
geediegram <geediegramgu@gmail.com>
2024-05-05 10:29:08 +0100

    g-dev update

--- src/Pages/Admin/users/createFieldOPerator.jsx
+++ src/Pages/Admin/users/createFieldOPerator.jsx
@@ -16,17 +16,6 @@ const CreateFieldOPerator = () => {
     })
 
     return (
-<<<<<<< HEAD
-        <RadaForm btnClass={'w-[fit-content]'} className={'flex flex-col justify-center'} validationSchema={schema} btnText={'Create Field Operator'} url={'/admin/create-fieldOperator'} method={'post'} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', width: '500px', gap: '20px' }} >
-            <Input name='superAdminEmail' hidden value={'kehindesalaudeen222@gmail.com'} />
-            <Input label={'First Name'} name='firstName' />
-            <Input label={'Last Name'} name='lastName' />
-            <Input label={'Email'} name='email' />
-            <Input label={'Asset'} name='assetType' />
-
-
-
-=======
         <RadaForm
             btnClass={'w-[fit-content]'}
             className={'flex flex-col justify-center'}
@@ -38,17 +27,8 @@ const CreateFieldOPerator = () => {
             <Input label={'Last Name'} name='lastName' />
             <Input label={'Username'} name='email' />
             <Input label={'Asset'} name='asset' />
-            <div style={{ display: 'flex', justifyContent: 'space-between', gap: '300px' }} >
-                {/* <Button width={'100px'} shadow onClick={() => window.location.pathname.includes('152') ? navigate('/152/register') : window.location.pathname.includes('147') ? navigate('/147/register') : navigate('/24/register')} >
-                    Register
-                </Button> */}
-                {/* <Button width={'100px'} shadow onClick={() => login({ email: '', "password": "" })} >
-                    Login
-                </Button> */}
-            </div>
 
 
->>>>>>> 2e597d89edd36066ddb874aa10cf7b4563846cc9
         </RadaForm>
     )
 }
