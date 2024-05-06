226ae85bcc23fc186dbebce7c1271cf65311c21e
kenicodex <kehindesalaudeen222@gmail.com>
2024-05-06 22:49:57 +0100

    actions

--- src/Pages/Admin/usersdata/index.jsx
+++ src/Pages/Admin/usersdata/index.jsx
@@ -5,23 +5,15 @@ import RadaTable from 'Components/RadaTable'
 import { forms } from 'Pages/User/dataform/formFields'
 import { useQuery } from 'react-query'
 
-const UserData = ({url, header, fn =()=> null}) => {
+const UserData = ({ url, header, fn = () => null, actions}) => {
   const columns = forms[header].fields.map(field => ({ name: field.label, key: field.name }))
 
-// const pd_columns = [
-//   {name:'',key:''},
-//   {name:'',key:''},
-//   {name:'',key:''},
-//   {name:'',key:''},
-//   {name:'',key:''},
-//   {name:'',key:''}, q
-// ]
 
   const { data } = useQuery(url)
   // const updatedColumns = 
   return (
     <div style={{ padding: '10px 0px 70px 0px' }}>
-      <RadaTable columns={columns} data={data} fn={fn} />
+      <RadaTable columns={columns} data={data} fn={fn}  actions={actions}/>
     </div >
   )
 }
