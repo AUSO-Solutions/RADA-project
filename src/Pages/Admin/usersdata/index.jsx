a92dce683746504abc394650e4c1fc038c15b5f6
kenicodex <kehindesalaudeen222@gmail.com>
2024-05-17 08:49:24 +0100

    fix

--- src/Pages/Admin/usersdata/index.jsx
+++ src/Pages/Admin/usersdata/index.jsx
@@ -20,7 +20,6 @@ const UserData = ({ url, header, fn = () => null, actions, idKey, ...props }) =>
   // const updatedColumns = 
   return (
     <div style={{ padding: '10px 0px 70px 0px' }}>
-      {addAssetType}
       <RadaTable columns={columns} data={data} fn={fn} actions={actions} idKey={idKey} {...props} />
     </div >
   )
