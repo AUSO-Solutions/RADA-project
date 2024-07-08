4a133764ba2a09ce0137dd30b283fa004c7bdb5a
Emmanuel Oludairo <emmanuel.oludairo@Newcross.com>
2024-07-08 16:14:39 +0100

    roles endpionts

--- functions/roles_and_permissions/index.jsx
+++ functions/roles_and_permissions/index.jsx
@@ -2,7 +2,8 @@
 const admin = require("firebase-admin");
 const logger = require("firebase-functions/logger");
 const { onCall, HttpsError } = require("firebase-functions/v2/https");
-const crypto = require('crypto')
+const crypto = require('crypto');
+const { request } = require("http");
 // const { getAuth, createUserWithEmailAndPassword } = require("firebase/auth");
 
 
@@ -13,18 +14,120 @@ const hash = (password) => {
 }
 
 const createRole = onCall(async (request) => {
-  
+    try{
+        let { data } = request;
+        const { roleName, permissions } = data;
+
+        const db = admin.firestore();
+        
+        const roleExists = (await db.collection('roles').where("roleName", "==", roleName).get())?.docs[0]?.exists;
+
+        if (roleExists) {
+            throw { code: 'already-exists', message: 'Role already exists' };
+        }
+
+        const roleId = crypto.randomBytes(8).toString("hex");
+
+        await db.collection('roles').doc(roleId).set({ id: roleId, roleName, permissions });
+        
+        return { status: 'success', message: 'Role created successfully', data: { roleName, permissions } }
+    } catch (error) {
+        logger.log('error ===> ', error);
+        throw new HttpsError(error?.code, error?.message);
+    }
 });
 
+const renameRole = onCall(async (request) => {
+    try{
+        let { data } = request;
+        const { roleId, newRoleName } = data;
+
+        const db = admin.firestore();
+        const roleRef = db.collection('roles').doc(roleId);
+        const role = await roleRef.get();
+
+        if (!role.exists) {
+            throw { code: 'not-found', message: 'Role not found' };
+        }
+
+        // const newRoleRef = db.collection('roles').doc(newRoleName);
+        const newRoleExist = (await db.collection('roles').where('roleName', '==', newRoleName).get())?.docs[0]?.exists;
+
+        if (newRoleExist) {
+            throw { code: 'already-exists', message: 'New role name already exists' };
+        }
+
+        await roleRef.update({roleName: newRoleName});
+
+        return { status: 'success', message: 'Role renamed successfully', data: { roleId, newRoleName } };
+    } catch (error) {
+        logger.log('error ===> ', error);
+        throw new HttpsError(error?.code, error?.message);
+    }
+});
+
+const deleteRole = onCall(async (request) => {
+    try {
+        let { data } = request;
+        const { roleId } = data;
+
+        const db = admin.firestore();
+        const roleRef = db.collection('roles').doc(roleId);
+        const role = await roleRef.get();
+
+        if (!role.exists) {
+            throw { code: 'not-found', message: 'Role note found' };
+        }
+
+        await roleRef.delete();
+
+        return { status: 'success', message: 'Role deleted successfully', data: { roleName } };
+    } catch (error) {
+        logger.log('error ===> ', error);
+        throw new HttpsError(error?.code, error?.message)
+    }
+});
+
+
 const getRoles = onCall(async ({ }) => {
-  
+    try {
+        const db = admin.firestore();
+        const res = await db.collection('roles').get();
+
+        const roles = res.docs.docs.map(doc => doc.data());
+
+        return { status: 'success', data: roles };
+    } catch (error) {
+        logger.log('error ===> ', error);
+        return { status: 'failed', error }
+    }
+})
+
+
+const getRole = onCall(async ({ data }) => {
+    try{
+        const { roleId } = data;
+        if (!roleId) {
+            throw { code: 'invalid-argument', message: 'RoleId is required' };
+        }
+
+        const db = admin.firestore();
+        const roleRef = db.collection('roles').doc(roleId);
+        const roleDoc = await roleRef.get();
+
+        if (!roleDoc.exists) {
+            throw { code: 'not-found', message: 'Role not found' };
+        }
+
+        const role = roleDoc.data();
+
+        return { status: 'success', data: role };
+    } catch (error) {
+        logger.log('error ===> ', error);
+        throw new HttpsError(error?.code || 'unknown', error?.message || 'Failed to fetch role');
+    }
 
 })
 
-const getRole = onCall(async ({ }) => {
-   
 
-})
-
-
-module.exports = { login, createUser, getUsers }
+module.exports = { createRole, renameRole, deleteRole, getRoles, getRole }
