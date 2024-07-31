2411c6447dd0814408ec96f4632e9592654bf06b
Gideon-Scrella <geediegramgu@gmail.com>
2024-07-31 03:13:53 +0100

    fixes

--- src/Pages/User/FieldDataCapture/DailyoOperation/ShippingRecords.jsx
+++ src/Pages/User/FieldDataCapture/DailyoOperation/ShippingRecords.jsx
@@ -37,13 +37,64 @@ const NoUnits = () => {
   </>
 }
 
+const DefineReport = ({ asset, }) => {
+  const dispatch = useDispatch()
+  const setupData = useSelector(state => state.setup)
+  const timeFrames = ["Daily", "Weekly", "Monthly"]
+  const reportTypes = useMemo(() => {
+    return [
+      { value: 'Gross Liquid', label: 'Gross Liquid' },
+      { value: 'Net Oil/ Condensate', label: 'Net Oil/ Condensate' },
+      { value: 'Gas', label: 'Gas' }
+    ]
+  }, [setupData?.reportTypes])
+  const handleCheck = (name, event) => {
+    const checked = event.target.checked
+    let selectedReportTypes = setupData?.reportTypes || []
+    if (checked) {
+      selectedReportTypes = Array.from(new Set([...selectedReportTypes, name]))
+    } else {
+      selectedReportTypes = selectedReportTypes.filter(reportType => reportType !== name)
+    }
+    dispatch(setSetupData({ name: 'reportTypes', value: selectedReportTypes }))
+  }
+
+  const checkAll = (e) => {
+    const checked = e.target.checked
+    dispatch(setSetupData({ name: 'reportTypes', value: [] }))
+    if (checked) dispatch(setSetupData({ name: 'reportTypes', value: reportTypes.filter(reportType => reportType.value !== 'All').map(reportType => reportType.value) }))
+  }
+  return <>
+    <div className='flex justify-between !w-[100%]'>
+      <Input type='select' placeholder={setupData?.asset} containerClass={'h-[39px] !w-[150px]'} defaultValue={asset} disabled />
+      <Input type='select' placeholder="Daily" containerClass={'h-[39px] !w-[150px]'}
+        defaultValue={{ label: setupData?.timeFrame, value: setupData?.timeFrame }}
+        onChange={(e) => dispatch(setSetupData({ name: 'timeFrame', value: e.value }))}
+        options={timeFrames?.map(timeFrame => ({ value: timeFrame, label: timeFrame }))} />
+    </div>
+
+    <div key={setupData?.reportTypes?.length} className='flex flex-col mt-[24px] rounded-[8px] gap-[24px] border'>
+      <div className='flex border-b px-3'>
+        <CheckInput defaultChecked={setupData?.reportTypes?.length === reportTypes.length} onChange={(e) => checkAll(e)} label={'Select Report Type'} />
+
+      </div>
+      {
+        reportTypes.map(repoortType => <div className='flex border-b px-3'>
+          <CheckInput defaultChecked={setupData?.reportTypes?.includes(repoortType.value)} onChange={(e) => handleCheck(repoortType.value, e)} label={repoortType.label} />
+        </div>)
+      }
+    </div>
+
+  </>
+}
+
 const ShippingRecords = () => {
   return (
     <>
       <Setup
         title={'Setup Shipping Records Parameters'}
         steps={["Shipping Method", "Define Report", "Preview"]}
-        stepComponents={[<NoUnits />,]}
+        stepComponents={[<NoUnits />, <DefineReport/>]}
       />
     </>
   )
