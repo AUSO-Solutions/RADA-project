b30aaab5de8f379553b4c2520372624025e8e41d
kenicodex <kehindesalaudeen222@gmail.com>
2024-08-11 12:23:43 +0100

    fixes

--- src/Pages/User/FieldDataCapture/DailyoOperation/VolumeMeasurement.jsx
+++ src/Pages/User/FieldDataCapture/DailyoOperation/VolumeMeasurement.jsx
@@ -92,23 +92,26 @@ const NoUnits = () => {
   const setupData = useSelector(state => state.setup)
   const { flowStations } = useAssetByName(setupData?.asset)
   const dispatch = useDispatch()
+  console.log(setupData, 'jj')
   useEffect(() => {
-    if (flowStations.length) {
+    if (flowStations.length && setupData?.flowStations?.length) {
       dispatch(setSetupData({
         name: 'flowStations',
-        value: flowStations.map((flowStation, i) => ({ ...setupData.flowStations[i], name: flowStation }))
+        value: flowStations.map((flowStation, i) => ({ ...setupData?.flowStations?.[i], name: flowStation }))
       }))
     }
   }, [flowStations])
   const updateFlowstation = (e, i) => {
-    let measurementType = e.value
-    let prevFlowstations = [...setupData?.flowStations]
-    prevFlowstations[i] = { ...prevFlowstations[i], measurementType }
-    if (measurementType) {
-      dispatch(setSetupData({
-        name: 'flowStations',
-        value: prevFlowstations
-      }))
+    if (setupData?.flowStations?.length) {
+      let measurementType = e.value
+      let prevFlowstations = [...setupData?.flowStations]
+      prevFlowstations[i] = { ...prevFlowstations[i], measurementType }
+      if (measurementType) {
+        dispatch(setSetupData({
+          name: 'flowStations',
+          value: prevFlowstations
+        }))
+      }
     }
   }
   return <>
@@ -126,11 +129,10 @@ const NoUnits = () => {
         flowStations.map((flowStation, i) => (<div className='flex justify-between p-3'>
           <Input containerClass={'h-[39px] !w-[fit-content]'} type='text' value={flowStation} disabled />
           <Input containerClass={'h-[39px] !w-[150px]'} type='select'
-            defaultValue={{ label: setupData?.flowStations[i].measurementType, value: setupData?.flowStations[i].measurementType }}
+            defaultValue={{ label: setupData?.flowStations?.[i]?.measurementType, value: setupData?.flowStations?.[i]?.measurementType }}
             onChange={e => updateFlowstation(e, i)}
             options={measureMenntTypes.map(type => ({ label: type, value: type }))} />
 
-          {setupData?.flowStations[i].measurementType}
         </div>))
       }
     </div>
@@ -184,8 +186,8 @@ const Preview = () => {
   return <>
     <div className='border mt-3 !rounded-[8px]'>
       <div className='flex justify-between border-b p-3'>
-        <Text weight={600}className='w-1/3' size={"16px"}>Flow stations</Text>
-        <Text weight={600} className={'!text-center w-1/3' } size={"16px"}>Type</Text>
+        <Text weight={600} className='w-1/3' size={"16px"}>Flow stations</Text>
+        <Text weight={600} className={'!text-center w-1/3'} size={"16px"}>Type</Text>
         <Text weight={600} className='w-1/3 !text-right' size={"16px"}>Number of Meters/Tanks</Text>
       </div>
       {
