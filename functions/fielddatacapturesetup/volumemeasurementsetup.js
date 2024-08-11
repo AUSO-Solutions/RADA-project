b3b2f18ac405a9f9e812890b7000c998aeda2f3c
kenicodex <kehindesalaudeen222@gmail.com>
2024-08-11 12:51:47 +0100

    fixes

--- functions/fielddatacapturesetup/volumemeasurementsetup.js
+++ functions/fielddatacapturesetup/volumemeasurementsetup.js
@@ -1,22 +1,29 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const { onCall, HttpsError } = require('firebase-functions/v1/https');
-admin.initializeApp();
-const db = admin.firestore();
+// const functions = require('firebase-functions');
+// const admin = require('firebase-admin');
+// const { onCall, HttpsError } = require('firebase-functions/v1/https');
+// const db = admin.firestore();
+
+
+const admin = require("firebase-admin");
+const logger = require("firebase-functions/logger");
+const { onCall, HttpsError } = require("firebase-functions/v2/https");
+const crypto = require('crypto');
+// const { currentTime } = require("../../helpers"); 
 
 const generateSerialNumbers = (count) => {
     const serialNumbers = [];
     for (let i = 0; i < count; i++) {
-        serialNumbers.push(`SN-${Math.random().toString(36).substr(2, 9)}`);
+        serialNumbers.push(`SN-${Math.random().toString(36).substring(2, 9)}`);
     }
     return serialNumbers;
 };
 
 const setupVolumeMeasurement = onCall(async (request) => {
+
     try {
         let { data } = request;
 
-        const { asset, reportTypes, flowStations, measurementTypeNumber, timeFrame, measurementType } = data;
+        const { asset, reportTypes, flowStations, timeFrame } = data;
 
         const validAssets = ['OML24', 'OML152', 'OML147'];
         if (!validAssets.includes(asset)) {
@@ -28,10 +35,10 @@ const setupVolumeMeasurement = onCall(async (request) => {
             return { message: 'Invalid report types', code: 'cancelled' };
         }
 
-        const validVolumeMeasurementTypes = ['Metering', 'Tank Dipping'];
-        if (!validVolumeMeasurementTypes.includes(measurementType)) {
-            return { message: 'Invalid measurement type', code: 'cancelled' };
-        }
+        // const validVolumeMeasurementTypes = ['Metering', 'Tank Dipping'];
+        // if (!validVolumeMeasurementTypes.includes(measurementType)) {
+        //     return { message: 'Invalid measurement type', code: 'cancelled' };
+        // }
 
         // if (![2, 3].includes(numberOfFlowStations)) {
         //     return { message: 'Invalid number of flow stations', code: 'cancelled' };
@@ -53,14 +60,14 @@ const setupVolumeMeasurement = onCall(async (request) => {
         //         meters: generateSerialNumbers(numberOfMeters)
         //     });
         // }
-    
-        const docRef = await db.collection('setups').doc('volumeMeasurement').collection("setupList").doc().set({
+        const id = generateSerialNumbers(6)
+        const db = admin.firestore();
+
+        const docRef = await db.collection('setups').doc('volumeMeasurement').collection("setupList").doc(id).set({
             asset,
             reportTypes,
             flowStations,
-            measurementType,
-            measurementTypeNumber,
-            timeFrame,
+            timeFrame, id
         });
 
         return { message: `Document created with ID: ${docRef.id}` };
