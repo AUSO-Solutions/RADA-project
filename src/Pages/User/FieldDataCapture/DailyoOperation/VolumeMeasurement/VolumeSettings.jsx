29dabd677b93c5ac72b635c52772a274c5d412a5
kenicodex <kehindesalaudeen222@gmail.com>
2024-08-13 07:32:41 +0100

    updates

---
+++ src/Pages/User/FieldDataCapture/DailyoOperation/VolumeMeasurement/VolumeSettings.jsx
@@ -0,0 +1,97 @@
+import { Box } from '@mui/material'
+import Text from 'Components/Text'
+import React from 'react'
+import { useSelector } from 'react-redux'
+// import { setSetupData } from 'Store/slices/setupSlice'
+import { updateFlowstation, updateFlowstationReading } from './helper'
+import { Button } from 'Components'
+import { firebaseFunctions } from 'Services'
+import { store } from 'Store'
+import { Close } from '@mui/icons-material'
+
+const VolumeSettings = ({ onClickOut = () => null }) => {
+  const setupData = useSelector(state => state.setup)
+  console.log(setupData)
+
+
+  const save = async () => {
+    try {
+
+      const _setupData = store.getState().setup
+      await firebaseFunctions('updateVolumeMeasurement', { ..._setupData })
+    } catch (error) {
+
+      console.log(error)
+    }
+  }
+
+
+  return (
+    <>
+      <div className='h-[100vh] w-[100vw] fixed ' onClick={onClickOut}></div>
+      <div className='fixed bottom-2 right-8 p-4 w-[500px] drop-shadow border p-2 rounded bg-white p'>
+
+        <div className='flex justify-between'>
+          <Text weight={600} size={18}>  Table Settings</Text>
+          <Close onClick={onClickOut} />
+        </div> <br />  <br />
+        <Text weight={600} size={14} color={'rgba(78, 78, 78, 0.5)'}>  Manage Meters</Text>
+        <br />
+        <Box className='!border-t  border-b border-b-black !border-t-black flex justify-between py-3' >
+          <Text weight={600} size={14} className={'w-[30%]'}>  Manage Meters</Text>
+          <Text weight={600} size={14} className={'w-[30%] !text-center'}> Meter/Tank Name</Text>
+          <Text weight={600} size={14} className={'w-[30%] !text-right'}>  Meter Factor</Text>
+        </Box>
+        {
+
+          setupData?.flowStations?.map((flowStation, flowStationIndex) => (
+            <Box className=' flex justify-between py-3 items-center border-b border-b-grey' >
+              <Text size={14} className={'w-[30%] flex items-center'}>  {flowStation?.name}</Text>
+              <Text size={14} className={'w-[30%] flex flex-col !text-center'}>
+                {
+                    new Array(parseInt(flowStation?.numberOfUnits)).fill(0).map((meter, readingIndex) => (
+                      <div>
+                        <input className='border outline-none px-2 w-[98px] h-[30px] rounded my-1'
+                          value={flowStation?.readings?.[readingIndex]?.serialNumber}
+                          onChange={(e) => updateFlowstationReading(flowStationIndex, readingIndex, 'serialNumber', e.target.value)}
+                        />
+                      </div>
+                    )) 
+                }
+                {
+                   flowStation?.measurementType === 'Tank Dipping' && "Deduction"
+                }
+              </Text>
+              <Text size={14} className={'w-[30%] !text-right'}>
+                {
+                   
+                    new Array(parseInt(flowStation?.numberOfUnits)).fill(0).map((meter, readingIndex) => (
+                      <div>
+                        <input type='number'
+                        disabled={flowStation?.measurementType === 'Tank Dipping'}
+                          value={flowStation?.measurementType === 'Metering' ? flowStation?.readings?.[readingIndex]?.meterFactor : 1}
+                          onChange={(e) => updateFlowstationReading(flowStationIndex, readingIndex, 'meterFactor', e.target.value)}
+                          min={1} className='border outline-none text-center px-2 w-[98px] h-[30px] my-1' />
+                      </div>
+                    )) 
+                }
+                {
+                   flowStation?.measurementType === 'Tank Dipping' &&  <input type='number'
+                   value={flowStation?.deductionMeterFactor}
+                   onChange={(e) => updateFlowstation(flowStationIndex, 'deductionMeterFactor', e.target.value)}
+                   min={1} className='border outline-none text-center px-2 w-[98px] h-[30px] my-1' />
+                }
+              </Text>
+            </Box>
+          ))
+        }
+
+        <Button className={'w-[200px] mx-auto mt-5 '} onClick={save}>
+          Save
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default VolumeSettings
