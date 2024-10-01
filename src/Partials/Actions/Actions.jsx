ee62729c90d1ca940294a652558b7eae9a37072a
kenicodex <kehindesalaudeen222@gmail.com>
2024-10-01 23:05:39 +0100

    fixes

--- src/Partials/Actions/Actions.jsx
+++ src/Partials/Actions/Actions.jsx
@@ -2,84 +2,14 @@ import { Button } from 'Components'
 import { ArrowDown2 } from 'iconsax-react'
 import React from 'react';
 import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-// import { useDispatch } from 'react-redux';
-// import { closeModal, openModal } from 'Store/slices/modalSlice';
-// import Text from 'Components/Text';
-// import { Avatar, Divider } from '@mui/material';
-// import { colors } from 'Assets';
-// import { useFetch } from 'hooks/useFetch';
-// import CheckInput from 'Components/Input/CheckInput';
-// import { toast } from 'react-toastify';
-// import { CheckOutlined } from '@mui/icons-material';
+import MenuItem from '@mui/material/MenuItem'; 
 
-
-// const SendTo = ({ title }) => {
-//     const dispatch = useDispatch()
-//     const { data: groups } = useFetch({ firebaseFunction: 'getGroups' })
-//     const [selectedGroups, setSelectedGroups] = React.useState([])
-//     const back = async () => {
-//         dispatch(closeModal())
-//         dispatch(openModal({ component: <Query /> }))
-//     }
-//     const selectGrroup = (group) => {
-//         setSelectedGroups(prev => {
-//             if (prev.map(group => group?.groupName).includes(group?.groupName)) {
-//                 return prev.filter(prevGroup => prevGroup?.groupName !== group?.groupName)
-//             }
-//             return [...prev, group]
-//         })
-//     }
-//     const send = async () => {
-//         console.log(selectedGroups)
-//         dispatch(closeModal())
-//         dispatch(openModal({ component: <Query done /> }))
-//     }
-//     return <div className='w-[500px] p-1 h-[600px]'>
-//         <Text size={24}>Send to</Text>
-
-//         <Divider className='!mt-[40px]' />
-
-//         <input type="search" className='border bg-[#FAFAFAFA] px-2 py-1 w-[200px] mt-4 rounded' placeholder='Search group' />
-//         <div className='mt-5 p-2  !h-[100px] f'>
-//             < div className='py-2 border-b'>
-//                 <CheckInput label={'Select all group'} key={selectedGroups.length} checked={selectedGroups.length === groups.length} onChange={(e) => {
-//                     setSelectedGroups(prev => {
-//                         console.log(prev.length === groups.length)
-//                         return prev.length === groups.length ? [] : groups
-//                     })
-//                 }} />
-//             </div>
-//             <div className='flex flex-col'>
-//                 {
-//                     groups.map(group => <div className='py-2 border-b'>
-//                         <CheckInput onChange={() => selectGrroup(group)} checked={selectedGroups.includes(group)} key={group?.groupName} label={group.groupName} />
-//                     </div>)
-//                 }
-//             </div>
-
-
-
-//             <Button onClick={back} color={colors.rada_blue} bgcolor={'white'} width={100} className={`float-left border !border-[${colors.rada_blue}] mt-[50px]`}>
-//                 Back
-//             </Button>
-//             <Button onClick={send} width={100} className={'float-right mt-[50px]'}>
-//                 Send
-//             </Button>
-//         </div>
-
-//     </div>
-// }
-
-
-export default function Actions({ wellTestResult, title , actions}) {
-    // console.log({ title })
+export default function Actions({ actions}) {
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
     };
-    // const dispatch = useDispatch()
 
     const handleClose = () => {
         setAnchorEl(null);
@@ -104,9 +34,7 @@ export default function Actions({ wellTestResult, title , actions}) {
                 {
                     actions.map(action =>  <MenuItem onClick={() => { handleClose(); action?.onClick() }}>{action?.name}</MenuItem>)
                 }
-                {/* <MenuItem onClick={() => { handleClose(); dispatch(openModal({ component: <Approve /> })) }}>Approve Result</MenuItem>
-                <MenuItem onClick={() => { handleClose(); dispatch(openModal({ component: <Query wellTestResult={wellTestResult} title={title} /> })) }}>Query Result</MenuItem> */}
-            </Menu>
+                          </Menu>
         </div>
     );
 }
--- src/Partials/ExcelToCSV.jsx
+++ src/Partials/ExcelToCSV.jsx
@@ -1,33 +1,54 @@
-import React, { useId, useState } from "react";
-import Papa from "papaparse";
+import React, { useId } from "react";
+// import Papa from "papaparse";
 import { read, utils } from "xlsx";
-import csvToJson from "utils/cvsToJson";
-import { toast } from "react-toastify";
+// import csvToJson from "utils/cvsToJson";
+// import { toast } from "react-toastify";
+// import dayjs from "dayjs";
 function ExcelToCsv({ onComplete = () => null, children, className }) {
-    const [, setCsvData] = useState("");
+    // const [, setCsvData] = useState("");
 
-    const handleFileUpload = (event) => {
-        const file = event.target.files[0];
-        if (file.type !== "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"){
-            toast.error('File must be excel')
-        }
+    // const handleFileUpload = (event) => {
+    //     const file = event.target.files[0];
+    //     if (file.type !== "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"){
+    //         toast.error('File must be excel')
+    //     }
+    //         const reader = new FileReader();
+    //     reader.onload = (e) => {
+    //         const data = new Uint8Array(e.target.result);
+    //         const workbook = read(data, { type: "array" });
+    //         const sheetName = workbook.SheetNames[0];
+    //         const sheet = workbook.Sheets[sheetName];
+    //         const csvData = Papa.unparse(utils.sheet_to_json(sheet), {
+    //             header: true,
+    //         });
+    //         setCsvData(csvData);
+    //         console.log(csvData)
+    //         csvToJson(csvData, onComplete)
+    //     };
+    //     (reader.readAsArrayBuffer(file));
+    // };
+
+
+
+    // console.log(dayjs("01/01/1970").add(45551,"days"))
+    const readUploadFile = (e) => {
+        e.preventDefault();
+        if (e.target.files) {
             const reader = new FileReader();
-        reader.onload = (e) => {
-            const data = new Uint8Array(e.target.result);
-            const workbook = read(data, { type: "array" });
-            const sheetName = workbook.SheetNames[0];
-            const sheet = workbook.Sheets[sheetName];
-            const csvData = Papa.unparse(utils.sheet_to_json(sheet), {
-                header: true,
-            });
-            // const newHeader = "Register No, Student Name, Branch, Semester, Course, Exam Type, Attendance, Withheld, IMark, Grade, Result\n";
-            // const finalCSVData = newHeader + csvData.substring(csvData.indexOf("\n") + 1)
-            setCsvData(csvData);
-            // console.log(csvData)
-            csvToJson(csvData, onComplete)
-        };
-        (reader.readAsArrayBuffer(file));
-    };
+            reader.onload = (e) => {
+                const data = e.target.result;
+                const workbook = read(data, { type: "array" });
+                const sheetName = workbook.SheetNames[0];
+                const worksheet = workbook.Sheets[sheetName];
+                const json = utils.sheet_to_json(worksheet, {
+                    raw: false,
+                });
+               onComplete(json)
+            };
+            reader.readAsArrayBuffer(e.target.files[0]);
+        }
+    }
+
     const id = useId()
     return (
         <div className={className} >
@@ -38,7 +59,7 @@ function ExcelToCsv({ onComplete = () => null, children, className }) {
                 type="file"
                 id={id}
                 hidden
-                onChange={handleFileUpload}
+                onChange={readUploadFile}
             />
             {/* <pre>{csvData}</pre> */}
         </div>
