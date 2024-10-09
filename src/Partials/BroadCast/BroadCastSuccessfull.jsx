04dbb083206db61a96e89fc7e0729e42e19a3648
kenicodex <kehindesalaudeen222@gmail.com>
2024-10-09 23:43:28 +0100

    fixes

--- src/Partials/BroadCast/BroadCastSuccessfull.jsx
+++ src/Partials/BroadCast/BroadCastSuccessfull.jsx
@@ -1,8 +1,20 @@
+import { CheckOutlined } from '@mui/icons-material'
+import { Avatar } from '@mui/material'
+import Text from 'Components/Text'
 import React from 'react'
 
 const BroadCastSuccessfull = () => {
   return (
-    <div>BroadCastSuccessfull</div>
+    <div className=' h-[200px] border gap-2 flex flex-col items-center justify-center p-2 rounded bg-[#F9FAFA]'>
+
+      <Avatar sx={{ bgcolor: '#00A3FF0D' }}>
+        <CheckOutlined sx={{ color: '#00A3FF' }} />
+      </Avatar>
+      <Text size={24}>
+      Broadcast Successful
+      </Text>
+      <Text align={'center'} className={'block w-[85%] text-center'}>Well Test DATA-OML99/Field1/Wells Schedule/July,2024 query has been sent successfully to the Field Engineers Group.</Text>
+    </div>
   )
 }
 
