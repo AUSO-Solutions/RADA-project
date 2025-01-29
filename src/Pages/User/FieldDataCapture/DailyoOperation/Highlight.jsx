ca9762411fa812d9383e138c9fdfd0735877586e
kenicodex <kehindesalaudeen222@gmail.com>
2025-01-29 17:12:22 +0100

    highlights notifiers

--- src/Pages/User/FieldDataCapture/DailyoOperation/Highlight.jsx
+++ src/Pages/User/FieldDataCapture/DailyoOperation/Highlight.jsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material'
 import { Button } from 'Components'
 import Text from 'Components/Text'
 import React, { useEffect, useState } from 'react'
-import { BsChevronRight } from 'react-icons/bs'
+import { BsChevronRight, BsCircleFill } from 'react-icons/bs'
 import { useDispatch, useSelector } from 'react-redux'
 import { toast } from 'react-toastify'
 import { openModal } from 'Store/slices/modalSlice'
@@ -10,6 +10,12 @@ import Editor from 'react-simple-wysiwyg';
 import { ArrowBack } from '@mui/icons-material'
 import { setDefaultHighlights, setHighlights } from 'Store/slices/highlightsSlice'
 
+
+// const highlightStatics = {
+//     production: { color: 'green' },
+//     operation: { color: 'yellow' },
+//     production: { color: 'blue' },
+// }
 
 const GoBack = ({ onBack = () => null }) => {
     return <><ArrowBack className='cursor-pointer absolute border !rounded-full shadow flex items-cennter justify-center top-2 b' onClick={onBack} /></>
@@ -31,16 +37,20 @@ const NoteBox = ({ onSave, onChange, highlightType, flowstation, captureData, on
         <div className=' flex flex-col w-[]  '>
             <GoBack onBack={() => onBack(highlightType, flowstation, html)} />
             <Editor value={html} onChange={(event) => {
-                setHtml(event.target.value)
-                onChange(event.target.value, highlightType, flowstation)
+                const value = (event.target.value === '<br>') ? '' : event.target.value
+                console.log(value)
+                setHtml(value)
+                onChange(value, highlightType, flowstation)
             }} />
             <Button className={'mt-2'} onClick={saveNote}>Save</Button>
         </div>
     )
 }
 
-
 const SelectFlowstation = ({ captureData, onSelect = () => null, }) => {
+    const highlightTypes = ['production', 'maintenance', 'operation']
+    const { data: highlights } = useSelector(state => state.highlights)
+
     return (
         <Box sx={{ width: "400px" }}>
             <Text size={18} weight={600} color={'grey'}> Select Flowstation</Text><br />
@@ -48,7 +58,15 @@ const SelectFlowstation = ({ captureData, onSelect = () => null, }) => {
                 {
                     Object.keys(captureData || {})?.map(flowstation => {
                         return <div onClick={() => onSelect(flowstation)} className='border rounded-[12px] cursor-pointer  w-full p-2 flex items-center justify-between'>
-                            <Text>{flowstation}</Text> <BsChevronRight />
+                            <Text>{flowstation}</Text>
+                            <div className='flex items-center'>
+                                <Text weight={600} className={'tracking-tighter'}>
+                                    {highlights?.[flowstation]?.production ? 'P ' : ''}
+                                    {highlights?.[flowstation]?.maintenance ? 'M' : ''}
+                                    {highlights?.[flowstation]?.operation ? ' O' : ''}
+                                </Text>
+                                <BsChevronRight size={18} />
+                            </div>
                         </div>
                     })
                 }
@@ -59,6 +77,8 @@ const SelectFlowstation = ({ captureData, onSelect = () => null, }) => {
 const SelectHighlightType = ({ onSelect = () => null, flowstation, onBack = () => null, captureData }) => {
 
     const highlightTypes = ['production', 'maintenance', 'operation']
+    const { data: highlights } = useSelector(state => state.highlights)
+    const prev = highlights?.[flowstation]
 
     return (
         <Box sx={{ width: "400px" }}>
@@ -69,9 +89,11 @@ const SelectHighlightType = ({ onSelect = () => null, flowstation, onBack = () =
                 {
                     highlightTypes.map(highlightType => {
                         return <div onClick={() => onSelect(highlightType, flowstation)} className='border rounded-[12px] cursor-pointer  !w-[100%] p-2 flex items-center justify-between'>
-                            <Text className={'capitalize'}>{highlightType} {
-                                // captureData[flowstation]?.highlight?.[highlightType] ? 
-                            } </Text> <BsChevronRight />
+                            <Text className={'capitalize'}>{highlightType}  </Text>
+                            <div className='flex items-center'>
+                                <BsCircleFill size={10} color={prev?.[highlightType] ? 'green' : "white"} />
+                                <BsChevronRight size={18} />
+                            </div>
                         </div>
                     })
                 }
@@ -80,7 +102,7 @@ const SelectHighlightType = ({ onSelect = () => null, flowstation, onBack = () =
     )
 }
 
-const Note = ({ title = "Highlight", onChange = () => null, captureData, onSave=()=>null }) => {
+const Note = ({ title = "Highlight", onChange = () => null, captureData, onSave = () => null }) => {
 
     const dispatch = useDispatch()
 
@@ -88,9 +110,9 @@ const Note = ({ title = "Highlight", onChange = () => null, captureData, onSave=
         const highlights = Object.fromEntries(Object.entries(captureData || {}).map(entry => [entry[0], entry[1]?.highlight]))
         dispatch(setDefaultHighlights(highlights))
         // console.log('--------')
-    }, [captureData,dispatch])
+    }, [captureData, dispatch])
 
-    const change = (highlight, highlightType, flowstation) => { 
+    const change = (highlight, highlightType, flowstation) => {
         dispatch(setHighlights({
             flowstation: flowstation,
             highlightType: highlightType,
